e84843d93a3fd9c0b7f1b1dd7e2a727c3d384023
mirzaipatrik <patrik.mirzai@gmail.com>
2024-07-25 14:25:42 +0300

    feat: remove next prefix from datocms api token

--- lib/datocms/datocms.tsx
+++ lib/datocms/datocms.tsx
@@ -7,7 +7,7 @@ interface RequestProps {
 export const performDatoCmsRequest = async ({ query, variables = {}, includeDrafts = false }: RequestProps) => {
     const response = await fetch("https://graphql.datocms.com/", {
         headers: {
-            Authorization: `Bearer ${process.env.NEXT_DATOCMS_API_TOKEN}`,
+            Authorization: `Bearer ${process.env.DATOCMS_API_TOKEN}`,
             ...(includeDrafts ? { "X-Include-Drafts": "true" } : {}),
         },
         method: "POST",
